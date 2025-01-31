43c52bb7480007c304220f469514434373b21d67
dzzdrzrx <166990749+dzzdrzrx@users.noreply.github.com>
2025-01-31 18:17:30 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+# essy pkg install.
+# what use it? terRpkg.
+# lol
+# I'll kill that bastard who made something exactly like mine after looking at mine.
+
+
+
+
+
+
+
+pkg install termux-api
+
+bash terRpkg.sh
+
+termux-toast -s pkg install success.
